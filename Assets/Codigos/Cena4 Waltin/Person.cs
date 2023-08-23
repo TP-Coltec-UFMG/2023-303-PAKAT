72df8d7f5cf0670926535a4d02ff4b4ea74335aa
Alisson Horta <104989804+AlissonHorta@users.noreply.github.com>
2023-08-23 17:30:50 -0300

    Finalizando a parte dos códigos

--- Assets/Codigos/Cena4 Waltin/Person.cs
+++ Assets/Codigos/Cena4 Waltin/Person.cs
@@ -6,20 +6,24 @@ public class Person : MonoBehaviour
 {
     private Rigidbody2D player;
     private float movePlayer;
-    public float speed;
-    private bool restartPlayer;
-    private GameObject inicialPos;
+    public float speed, alturaCamera, speedPorteira;
+    private bool restartPlayer, porteira;
+    private GameObject inicialPos, cameraPos;
+    public GameObject panelVitoria;
 
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        cameraPos = GameObject.Find("Main Camera");
         inicialPos = GameObject.Find("inicialPos");
+        porteira = false;
     }
 
     void Update()
     {
-        print(restartPlayer);
+        print(porteira);
         movePlayer = Input.GetAxis("Horizontal");
+        cameraPos.transform.position = new Vector3(cameraPos.transform.position.x, player.transform.position.y+alturaCamera, cameraPos.transform.position.z);
         player.velocity = new Vector2(movePlayer * speed, player.velocity.y);
         Restart(); 
     }
@@ -28,13 +32,22 @@ public class Person : MonoBehaviour
         if (col.CompareTag("armadilha")==true) {
             restartPlayer = true;
         }
+        if (col.CompareTag("vitoria")==true) {
+            porteira = true;
+        }
     }
 
     private void Restart() {
         if (restartPlayer == true) {
-            player.transform.position = new Vector2(inicialPos.transform.position.x, inicialPos.transform.position.y
-            );
+            player.transform.position = new Vector2(inicialPos.transform.position.x, inicialPos.transform.position.y);
             restartPlayer = false;
+        }
+    }
+
+    private void ProxFase () {
+        if (porteira==true) {
+            player.velocity = new Vector2(0, player.velocity.y);
+            panelVitoria.transform.position = Vector2.MoveTowards(panelVitoria.transform.position, cameraPos.transform.position, speedPorteira * Time.deltaTime);
         }
     }
 }
