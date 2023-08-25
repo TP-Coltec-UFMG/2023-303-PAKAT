0399ca6d64435cac75925a3aad7d98e4a3949ed3
KaioFFN <104567356+KaioFFN@users.noreply.github.com>
2023-08-25 11:23:49 -0300

    Fase Kaio

--- Assets/Codigos/cena 5 kl/PersonagemPular.cs
+++ Assets/Codigos/cena 5 kl/PersonagemPular.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PersonagemPular : MonoBehaviour
 {
     public float jumpForce = 10f;
     private bool canJump = true;
     private Rigidbody2D rb;
-
+    [SerializeField] GameObject GameOver;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,6 +22,10 @@ public class PersonagemPular : MonoBehaviour
         }
     }
 
+    public void ProximaCena(string nome)
+    {
+        SceneManager.LoadScene(nome);
+    }
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -29,12 +34,21 @@ public class PersonagemPular : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("chão"))
+        if (collision.gameObject.CompareTag("barreira"))
         {
             canJump = true;
         }
 
+        if (collision.gameObject.CompareTag("obstaculo"))
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (collision.gameObject.CompareTag("vitoria"))
+        {
+            SceneManager.LoadScene("CenaFinal");
+        }
+
     }
-    
-   
+
 }
