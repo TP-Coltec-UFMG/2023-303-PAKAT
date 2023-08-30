db7d866c5f4afddf7c6e13d90dfc286d0811f6b4
KaioFFN <104567356+KaioFFN@users.noreply.github.com>
2023-08-30 15:55:49 -0300

    Modificações ostaculos

---
+++ Assets/Codigos/cena 5 kl/Obst.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst: MonoBehaviour {
+    [SerializeField] private float velocidade = 5f;
+    [SerializeField] private float variacaoDaPosicaoX = 1;
+
+    private void Awake(){
+        this.transform.Translate(Vector3.left * Random.Range(-variacaoDaPosicaoX, variacaoDaPosicaoX));
+    }
+
+    private void Update() {
+        this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D outro){
+        Debug.Log("Bateu em: " + outro.gameObject.tag);
+        if(outro.gameObject.tag == "barreira")
+            this.Destruir();
+        // else if(outro.gameObject.tag == "Player")
+        //     Destroy(outro.gameObject);
+    }
+
+    public void Destruir(){
+        Destroy(this.gameObject);
+    }
+}
