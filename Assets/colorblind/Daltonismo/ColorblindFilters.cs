32f312c724e2819163320bef633c46ed3071435a
KaioFFN <104567356+KaioFFN@users.noreply.github.com>
2023-09-05 17:11:04 -0300

    Ativando dautonismo Menu

--- Assets/colorblind/Daltonismo/ColorblindFilters.cs
+++ Assets/colorblind/Daltonismo/ColorblindFilters.cs
@@ -6,74 +6,28 @@ using UnityEngine.UI;
 
 public class ColorblindFilters : MonoBehaviour
 {
-
-
-    public Toggle toggleNone;
-    public Toggle toggleProtanopia;
-    public Toggle toggleDeuteranotopia;
     public CameraController cam;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main.GetComponent<CameraController>();
-        if (PlayerPrefs.GetInt("ToggleBool") == 1)
-        {
-            toggleNone.isOn = true;
-        }
-        else
-        {
-            toggleNone.isOn = false;
-        } 
-        if (PlayerPrefs.GetInt("ToggleBool2") == 1)
-        {
-            toggleProtanopia.isOn = true;
-        }
-        else
-        {
-            toggleProtanopia.isOn = false;
-        }
-        if (PlayerPrefs.GetInt("ToggleBool3") == 1)
-        {
-            toggleDeuteranotopia.isOn = true;
-        }
-        else
-        {
-            toggleDeuteranotopia.isOn = false;
-        }
+        
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void ativarNone() 
     {
-        if (toggleNone.isOn == true)
-        {
-            PlayerPrefs.SetInt("ToggleBool", 1);
-            cam.filter.mode = ColorBlindMode.Normal;
-        }
-        else
-        {
-            PlayerPrefs.SetInt("ToggleBool", 0);
-        }
-        if (toggleProtanopia.isOn == true)
-        {
-            PlayerPrefs.SetInt("ToggleBool2", 1);
-            cam.filter.mode = ColorBlindMode.Protanopia;
-        }
-        else
-        {
-            PlayerPrefs.SetInt("ToggleBool2", 0);
-        }
-        if (toggleDeuteranotopia.isOn == true)
-        {
-            PlayerPrefs.SetInt("ToggleBool3", 1);
-            cam.filter.mode = ColorBlindMode.Deuteranopia;
-        }
-        else
-        {
-            PlayerPrefs.SetInt("ToggleBool3", 0);
-        }
+        cam.filter.mode = ColorBlindMode.Normal;
     }
+    public void ativarDeuteranotopia()
+    {
+        cam.filter.mode = ColorBlindMode.Deuteranopia;
+    }
+    public void ativarProtanopia()
+    {
+        cam.filter.mode = ColorBlindMode.Protanopia;
+    }
+    // Update is called once per frame
+ 
 
 }
