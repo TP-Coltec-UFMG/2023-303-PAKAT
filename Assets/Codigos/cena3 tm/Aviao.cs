8c4388e6ca420ef1047c5bfc8336f631cc800ac0
ArthurMacielSs <104994335+ArthurMacielSs@users.noreply.github.com>
2023-09-05 15:26:34 -0300

    cena funcionando

--- Assets/Codigos/cena3 tm/Aviao.cs
+++ Assets/Codigos/cena3 tm/Aviao.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Aviao : MonoBehaviour {
-    private Rigidbody2D fisica;
+    private Rigidbody2D player;
     [SerializeField] private float forca;
-    private Diretor diretor;
-    private Vector3 posicaoInicial;
+    //private Diretor diretor;
+    //private Vector3 posicaoInicial;
+
+
+
+    private bool restartPlayer, win;
+    public GameObject panelWin;
+    public GameObject GameOver; 
+    private GameObject inicialPos;
+    
     private bool  isPausa;
     public GameObject pausaPainel; 
 
     private void Start(){
-        this.posicaoInicial = this.transform.position;
-        this.fisica = this.GetComponent<Rigidbody2D>();
+        Time.timeScale = 1f;
+        player = GetComponent<Rigidbody2D>();
+        inicialPos = GameObject.Find("inicialPos");
+        win = false;
     }
 
   
@@ -21,6 +31,8 @@ public class Aviao : MonoBehaviour {
         if (!isPausa) {
         if(Input.GetButtonDown("Fire1")){
             this.Impulsionar();
+            Restart();
+            ProxFase(); 
         }
         }
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -29,20 +41,13 @@ public class Aviao : MonoBehaviour {
 
     }
 
-    public void Reiniciar(){
-        this.transform.position = this.posicaoInicial;
-        this.fisica.simulated = true;
-    }
 
     private void Impulsionar(){
-        this.fisica.velocity = Vector2.zero; //a gravidade se acumula no objeto quando está em baixo então ele não sobe muito e os cliks são inconstantes 
-        this.fisica.AddForce(Vector2.up * this.forca,ForceMode2D.Impulse);
+        this.player.velocity = Vector2.zero; //a gravidade se acumula no objeto quando está em baixo então ele não sobe muito e os cliks são inconstantes 
+        this.player.AddForce(Vector2.up * this.forca,ForceMode2D.Impulse);
     }
 
-    private void OnCollisionEnter2D(Collision2D colisao){
-        this.fisica.simulated = false;
-        this.diretor.FinalizarJogo();
-    }
+ 
 
 void PauseScreen() {
         
@@ -60,6 +65,41 @@ void PauseScreen() {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true; 
         }
+        
   
 }
+
+private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("armadilha"))
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (collision.gameObject.CompareTag("vitoria"))
+        {
+            win = true;
+            Debug.Log(" vitoria");
+
+        }
+
+    }
+ private void Restart() {
+        if (restartPlayer == true) {
+            player.transform.position = new Vector2(inicialPos.transform.position.x, inicialPos.transform.position.y);
+            GameOver.SetActive(true);
+            Time.timeScale = 0;
+            restartPlayer = false;
+        }
+    }
+  private void ProxFase () {
+        if (win == true) {
+            player.velocity = new Vector2(0, player.velocity.y);
+            panelWin.SetActive(true);
+            Time.timeScale = 0;
+            win = false;
+            //panelWin.transform.position = Vector2.MoveTowards(panelwin.transform.position, cameraPos.transform.position, speedwin * Time.deltaTime);
+        }
+    }
 }
