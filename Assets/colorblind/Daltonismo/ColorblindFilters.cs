b342ce06ceaa3981cc498a470350e3dd6a87b2e1
KaioFFN <104567356+KaioFFN@users.noreply.github.com>
2023-09-06 12:24:13 -0300

    Daltonismo

--- Assets/colorblind/Daltonismo/ColorblindFilters.cs
+++ Assets/colorblind/Daltonismo/ColorblindFilters.cs
@@ -7,27 +7,55 @@ using UnityEngine.UI;
 public class ColorblindFilters : MonoBehaviour
 {
     public CameraController cam;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main.GetComponent<CameraController>();
-        
+        if (PlayerPrefs.HasKey("CameraFilterMode"))
+        {
+            // Recupere o valor salvo da PlayerPrefs e defina-o na câmera
+            int filterMode = PlayerPrefs.GetInt("CameraFilterMode");
+            if (filterMode == 0)
+            {
+                ativarNone();
+
+            }else if (filterMode == 1)
+            {
+                ativarDeuteranotopia();
+            
+            }else if(filterMode == 2)
+            {
+                ativarProtanopia();
+
+            }
+        }
+
     }
 
     public void ativarNone() 
     {
         cam.filter.mode = ColorBlindMode.Normal;
+        PlayerPrefs.SetInt("CameraFilterMode", 0);
+        PlayerPrefs.Save();
+
     }
     public void ativarDeuteranotopia()
     {
         cam.filter.mode = ColorBlindMode.Deuteranopia;
+        PlayerPrefs.SetInt("CameraFilterMode", 1);
+        PlayerPrefs.Save();
+
     }
     public void ativarProtanopia()
     {
         cam.filter.mode = ColorBlindMode.Protanopia;
+        PlayerPrefs.SetInt("CameraFilterMode", 2);
+        PlayerPrefs.Save();
+
     }
-    // Update is called once per frame
+    
  
 
 }
